91d28f41dec2cc6776820b7b16833bc4f8b5330c
Mohammed Atiq <aatiq4009@gmail.com>
2026-06-10 17:13:18 +0530

    vercel deploye

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/api-proxy/:path*",
+        destination: `${process.env.API_BASE_URL ?? "http://20.233.200.211:8001"}/:path*`,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
